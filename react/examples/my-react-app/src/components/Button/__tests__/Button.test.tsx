f979e57c402fd2fbe8a8cc5edd547556e4621246
HuongLe <huong.le@asnet.com.vn>
2023-04-20 16:30:22 +0700

    #3 Refactor: add toMatchSnapshot to create a snapshot test

--- react/examples/my-react-app/src/components/Button/__tests__/Button.test.tsx
+++ react/examples/my-react-app/src/components/Button/__tests__/Button.test.tsx
@@ -5,13 +5,13 @@ import '@testing-library/jest-dom/extend-expect';
 
 import { Button } from '../Button';
 
-describe('Testing button compoennt', () => {
+describe('Testing button component', () => {
   test('renders a button element', () => {
-    render(
+    const tree = render(
       <Button label='Click me' primary={false} size='medium' backgroundColor='a' state={'a'} />,
     );
     const buttonElement = screen.getByRole('button');
-
+    expect(tree).toMatchSnapshot();
     expect(buttonElement).toBeInTheDocument();
   });
 
