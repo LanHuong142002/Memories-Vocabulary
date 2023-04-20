11bacf84d2fb5d5da7ac192b9c847bd704a07212
HuongLe <huong.le@asnet.com.vn>
2023-04-20 11:47:41 +0700

    #3 Refactor: add example of test component

---
+++ react/examples/my-react-app/jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.svg$': 'svg-jest',
+    '.+\\.(css|png|webp|)$': 'jest-transform-stub',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '^uuid$': require.resolve('uuid'),
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.stories.tsx',
+    '!**/*.config.ts',
+    '!**/node_modules/**',
+    '!**/mocks/**',
+    '!**/constants/**',
+    '!**/types/**',
+    '!**/styles/**',
+  ],
+};
+
+export default config;
