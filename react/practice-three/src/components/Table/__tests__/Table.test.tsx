368b57b6b37ae23d37c32a629231aa922660a3c8
Huong Le <huong.le@asnet.com.vn>
2023-05-14 21:53:29 +0700

    #16 Refactor: update description unit test for table component

--- react/practice-three/src/components/Table/__tests__/Table.test.tsx
+++ react/practice-three/src/components/Table/__tests__/Table.test.tsx
@@ -2,30 +2,25 @@ import { render } from '@testing-library/react';
 import { Table } from '@components';
 
 describe('Testing Table component', () => {
-  it('Should render children', () => {
-    const { getByRole } = render(
-      <Table>
-        <tr>
-          <td>Cell 1</td>
-          <td>Cell 2</td>
-        </tr>
-      </Table>,
-    );
+  const children = (
+    <tr>
+      <td>Cell 1</td>
+      <td>Cell 2</td>
+    </tr>
+  );
+
+  it('Should render children correctly', () => {
+    const { getByRole } = render(<Table>{children}</Table>);
     const tableRow = getByRole('row');
+
     expect(tableRow).toBeInTheDocument();
     expect(tableRow.children).toHaveLength(2);
   });
 
   it('Should have the correct class name', () => {
-    const { getByRole } = render(
-      <Table>
-        <tr>
-          <td>Cell 1</td>
-          <td>Cell 2</td>
-        </tr>
-      </Table>,
-    );
+    const { getByRole } = render(<Table>{children}</Table>);
     const table = getByRole('table');
+
     expect(table).toHaveClass('table-wrapper');
   });
 });
--- react/practice-three/src/components/Table/__tests__/TableBody.test.tsx
+++ react/practice-three/src/components/Table/__tests__/TableBody.test.tsx
@@ -1,30 +1,24 @@
 import { render } from '@testing-library/react';
 import { TableBody } from '@components';
 
-describe('TableBody', () => {
+describe('Testing Table body component', () => {
+  const children = (
+    <tr>
+      <td>Cell 1</td>
+      <td>Cell 2</td>
+    </tr>
+  );
+
   it('Should renders children correctly', () => {
-    const { getByText } = render(
-      <TableBody>
-        <tr>
-          <td>Cell 1</td>
-          <td>Cell 2</td>
-        </tr>
-      </TableBody>,
-    );
+    const { getByText } = render(<TableBody>{children}</TableBody>);
 
     expect(getByText('Cell 1')).toBeInTheDocument();
     expect(getByText('Cell 2')).toBeInTheDocument();
   });
 
   it('Should renders class name correctly', () => {
-    const { container } = render(
-      <TableBody>
-        <tr>
-          <td>Cell 1</td>
-          <td>Cell 2</td>
-        </tr>
-      </TableBody>,
-    );
+    const { container } = render(<TableBody>{children}</TableBody>);
+
     expect(container.firstChild).toHaveClass('table-body');
   });
 });
