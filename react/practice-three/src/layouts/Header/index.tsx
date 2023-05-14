1487ef28e0a6c8d142d48ce1b2aea397f6ec7184
Huong Le <huong.le@asnet.com.vn>
2023-05-14 22:08:53 +0700

    #16 Refactor: update and add unit test for header component

--- react/practice-three/src/layouts/Header/index.tsx
+++ react/practice-three/src/layouts/Header/index.tsx
@@ -7,7 +7,7 @@ import { Typography } from '@components';
 const Header = () => {
   return (
     <header className='header-wrapper'>
-      <Typography text='Management' tagName='h1' weight='bold' color='tertiary' size='lg' />
+      <Typography text='Management' tagName='h1' weight='bold' color='quaternary' size='xl' />
     </header>
   );
 };
