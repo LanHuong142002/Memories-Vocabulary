da468540de30dec7b2e2723777835f00975e6c92
HuongLe <huong.le@asnet.com.vn>
2023-05-15 10:00:33 +0700

    #16 Refactor: remove context in action menu component

--- react/practice-three/src/pages/HomePage/components/ActionMenu/__tests__/ActionMenu.test.tsx
+++ react/practice-three/src/pages/HomePage/components/ActionMenu/__tests__/ActionMenu.test.tsx
@@ -1,6 +1,5 @@
 import { render, fireEvent } from '@testing-library/react';
 import { ActionMenu } from '@pages';
-import { Context, ModalContext } from '@contexts';
 
 describe('Testing Action Menu component', () => {
   const onDelete = jest.fn();
@@ -16,28 +15,11 @@ describe('Testing Action Menu component', () => {
   });
 
   it('Should call onDelete and show notification modal when Delete button is clicked', () => {
-    const contextValue: Context = {
-      showHideItemModal: jest.fn(),
-      showHideNotificationModal: jest.fn(),
-      showHideErrorsModal: jest.fn(),
-      itemModal: false,
-      notificationModal: false,
-      errorsModal: {
-        status: false,
-        message: '',
-      },
-    };
-
-    const { getByText } = render(
-      <ModalContext.Provider value={contextValue}>
-        <ActionMenu onEdit={onEdit} onDelete={onDelete} />
-      </ModalContext.Provider>,
-    );
+    const { getByText } = render(<ActionMenu onEdit={onEdit} onDelete={onDelete} />);
     const deleteButton = getByText('Delete');
 
     fireEvent.click(deleteButton);
 
-    expect(contextValue.showHideNotificationModal).toHaveBeenCalled();
     expect(onDelete).toHaveBeenCalled();
   });
 });
--- react/practice-three/src/pages/HomePage/components/ActionMenu/index.tsx
+++ react/practice-three/src/pages/HomePage/components/ActionMenu/index.tsx
@@ -1,13 +1,10 @@
-import { forwardRef, useCallback, useContext } from 'react';
+import { forwardRef } from 'react';
 
 // Styles
 import './index.css';
 
 // Components
 import { Button } from '@components';
-
-// Context
-import { ModalContext } from '@contexts';
 
 interface ActionMenuProps {
   onEdit: () => void;
@@ -18,26 +15,10 @@ const ActionMenu = forwardRef<HTMLDivElement, ActionMenuProps>(function ActionMe
   { onDelete, onEdit },
   ref,
 ) {
-  const { showHideNotificationModal } = useContext(ModalContext);
-
-  /**
-   * @description function handle action delete button
-   */
-  const handleDelete = useCallback(() => {
-    showHideNotificationModal();
-    onDelete();
-  }, [onDelete]);
-
   return (
     <div className='action-menu-wrapper' ref={ref}>
       <Button label='Edit' color='default' type='button' onClick={onEdit} variant='primary' />
-      <Button
-        label='Delete'
-        color='warning'
-        type='button'
-        onClick={handleDelete}
-        variant='primary'
-      />
+      <Button label='Delete' color='warning' type='button' onClick={onDelete} variant='primary' />
     </div>
   );
 });
