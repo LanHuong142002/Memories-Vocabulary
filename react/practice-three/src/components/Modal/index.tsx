a3fa55bfe8a61ce801fe06071159e39cc5176e16
Huong Le <huong.le@asnet.com.vn>
2023-05-15 10:15:28 +0700

    #16 Refactor: update modal component and add unit test

--- react/practice-three/src/components/Modal/index.tsx
+++ react/practice-three/src/components/Modal/index.tsx
@@ -4,22 +4,34 @@ import { ReactNode } from 'react';
 import './index.css';
 
 // Images
-import Cancel from '@assets/icons/cancel.svg';
+import Cancel from '@assets/icons/cancel-icon.svg';
 
 // Components
 import { Image } from '@components';
 
 interface ModalProps {
   children: ReactNode;
+  title?: string;
+  icon?: string;
   toggleModal: () => void;
 }
 
-const Modal = ({ children, toggleModal }: ModalProps) => {
+const Modal = ({ title, icon, children, toggleModal }: ModalProps) => {
   return (
     <div className='overlay'>
       <div className='modal-wrapper'>
         <div className='modal-header'>
-          <Image image={Cancel} size='sm' onClick={toggleModal} isCursorPointer={true} />
+          <div className='modal-icon'>
+            {icon && <Image size='lg' alt='icon' url={icon} />}
+            {title && <p>{title}</p>}
+          </div>
+          <Image
+            url={Cancel}
+            size='xxxs'
+            onClick={toggleModal}
+            isClickable={true}
+            alt='icon cancel'
+          />
         </div>
         <div className='modal-body'>{children}</div>
       </div>
