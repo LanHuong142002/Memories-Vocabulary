9fc397869f6fb15514e5648977cd0ab1eb375dec
Huong Le <huong.le@asnet.com.vn>
2023-05-16 17:40:16 +0700

    #17 Refactor: update test case for validation

--- react/practice-three/src/helpers/__tests__/validation.test.ts
+++ react/practice-three/src/helpers/__tests__/validation.test.ts
@@ -2,50 +2,56 @@
 import { MESSAGE_ERRORS } from '@constants';
 
 // Helpers
-import { checkEmpty, checkNumber, validation } from '@helpers';
+import { isEmpty, isPositiveNumber, isRegex, validation } from '@helpers';
 
-describe('Testing checkEmpty', () => {
-  it('Should return an error message if the input is empty', () => {
-    const value = checkEmpty('');
-
-    expect(value).toEqual(MESSAGE_ERRORS.EMPTY_FIELD);
+describe('Testing isEmpty function', () => {
+  it('Should return true empty string', () => {
+    expect(!isEmpty('')).toBe(true);
   });
 
-  it('Should return an empty string if the input is not empty', () => {
-    const value = checkEmpty('hello');
-
-    expect(value).toEqual('');
-  });
-});
-
-describe('Testing checkNumber', () => {
-  it('Should return an error message if the input empty', () => {
-    const value = checkNumber('name', '');
-
-    expect(value).toEqual(MESSAGE_ERRORS.EMPTY_FIELD);
+  it('Should return true string with only spaces', () => {
+    expect(!isEmpty('   ')).toBe(true);
   });
 
-  it('Should return an error message if the value is not positive number', () => {
-    const value = checkNumber('name', '-2');
-
-    expect(value).toEqual(MESSAGE_ERRORS.POSITIVE_NUMBER);
-  });
-
-  it('Should return an empty string if the input is not empty', () => {
-    const value = checkNumber('name', '123');
-
-    expect(value).toEqual('');
-  });
-
-  it('Should return an error if the input is a decimal and the key is the quantity', () => {
-    const value = checkNumber('quantity', '123.2');
-
-    expect(value).toEqual(MESSAGE_ERRORS.INTEGER_NUMBER);
+  it('Should return true string with value characters', () => {
+    expect(isEmpty('hello')).toBe(true);
   });
 });
 
-describe('Testing validation', () => {
-  it('should return an object with error messages for empty fields', () => {
+describe('Testing isRegex function', () => {
+  const regex = /[a-z]/;
+
+  it('Should return true if the value matches the regular expression', () => {
+    const value = 'hello';
+    expect(isRegex(regex, value)).toBe(false);
+  });
+
+  it('Should return false if the value does not match the regular expression', () => {
+    const value = '123';
+    expect(isRegex(regex, value)).toBe(true);
+  });
+
+  it('Should return true if the value is an empty string', () => {
+    const value = '';
+    expect(isRegex(regex, value)).toBe(true);
+  });
+});
+
+describe('Testing isPositiveNumber function', () => {
+  it('Should return true for positive numbers', () => {
+    expect(isPositiveNumber(1)).toBe(true);
+    expect(isPositiveNumber(0)).toBe(true);
+    expect(isPositiveNumber(0.1)).toBe(true);
+  });
+
+  it('Should return false for negative numbers', () => {
+    expect(isPositiveNumber(-1)).toBe(false);
+    expect(isPositiveNumber(-0.1)).toBe(false);
+  });
+});
+
+describe('Testing validation function', () => {
+  it('Should return an object with error messages for empty fields', () => {
     const data = {
       name: '',
       email: '',
@@ -56,23 +62,65 @@ describe('Testing validation', () => {
       email: MESSAGE_ERRORS.EMPTY_FIELD,
       age: '',
     };
-    const result = validation(data);
+    const result = validation<typeof data, Record<keyof typeof data, string>>(data);
 
     expect(result).toEqual(expected);
   });
 
-  it('should return an object with error messages for fields that should be numbers', () => {
+  it('Should return an object with error messages for fields that should be numbers', () => {
     const data = {
       name: 'John Doe',
       email: 'john.doe@example.com',
       age: '-2',
+      quantity: 'abc',
     };
     const expected = {
       name: '',
       email: '',
       age: MESSAGE_ERRORS.POSITIVE_NUMBER,
+      quantity: MESSAGE_ERRORS.INTEGER_NUMBER,
     };
-    const result = validation(data, ['age']);
+    const result = validation<typeof data, Record<keyof typeof data, string>>(data, [
+      'age',
+      'quantity',
+    ]);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('Should return an object with error messages for non-empty fields', () => {
+    const data = {
+      name: 'John Doe',
+      email: '',
+      age: '',
+    };
+    const expected = {
+      name: '',
+      email: MESSAGE_ERRORS.EMPTY_FIELD,
+      age: MESSAGE_ERRORS.EMPTY_FIELD,
+    };
+    const result = validation<typeof data, Record<keyof typeof data, string>>(data);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('Should return an object with no error messages for valid input', () => {
+    const data = {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      age: '',
+      quantity: '10',
+    };
+    const expected = {
+      name: '',
+      email: '',
+      age: MESSAGE_ERRORS.EMPTY_FIELD,
+      quantity: '',
+    };
+    const result = validation<typeof data, Record<keyof typeof data, string>>(data, [
+      'age',
+      'quantity',
+    ]);
 
     expect(result).toEqual(expected);
   });
