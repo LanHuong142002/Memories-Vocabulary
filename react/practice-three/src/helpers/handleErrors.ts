e511c2339a983cfc6c4340fcaf2f2eb8ddd5157e
Huong Le <huong.le@asnet.com.vn>
2023-05-17 17:35:24 +0700

    #25 Refactor: update function customMessageErrors and add class ResponseError

--- react/practice-three/src/helpers/handleErrors.ts
+++ react/practice-three/src/helpers/handleErrors.ts
@@ -1,10 +1,5 @@
 import { STATUS_CODE } from '@constants';
 
-type CustomErrors = {
-  messageError: string;
-  status: number;
-};
-
 /**
  * @description function custom error based on status code from
  * response and item if this status is success
@@ -14,27 +9,26 @@ type CustomErrors = {
  *
  * @returns {Object}
  */
-const customErrors = <T>(response: Response, items: T): T => {
+const customMessageErrors = <T>(response: Response, items: T): string | T => {
   switch (response.status) {
     case STATUS_CODE.OK:
     case STATUS_CODE.CREATED:
       return items;
     case STATUS_CODE.BAD_REQUEST:
-      throw { messageError: `${response.status} Bad Request`, status: response.status };
+      return `${response.status} Bad Request`;
     case STATUS_CODE.UNAUTHORIZED:
-      throw { messageError: `${response.status} Unauthorized`, status: response.status };
+      return `${response.status} Unauthorized`;
     case STATUS_CODE.FORBIDDEN:
-      throw { messageError: `${response.status} Forbidden`, status: response.status };
+      return `${response.status} Forbidden`;
     case STATUS_CODE.NOT_FOUND:
-      throw { messageError: `${response.status} Page Not Found`, status: response.status };
+      return `${response.status} Page Not Found`;
     case STATUS_CODE.INTERNAL_SERVER_ERROR:
-      throw { messageError: `${response.status} Internal Server Error`, status: response.status };
+      return `${response.status} Internal Server Error`;
     case STATUS_CODE.SERVER_UNAVAILABLE:
-      throw { messageError: `${response.status} Service Unavailable`, status: response.status };
+      return `${response.status} Service Unavailable`;
     default:
-      throw { messageError: `${response.status} Fail to fetch`, status: response.status };
+      return `${response.status} Fail to fetch`;
   }
 };
 
-export { customErrors };
-export type { CustomErrors };
+export { customMessageErrors };
--- react/practice-three/src/helpers/index.ts
+++ react/practice-three/src/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './handleErrors';
 export * from './convert';
+export { default as ResponseError } from './ResponseError';
 export { default as validation } from './validators';
