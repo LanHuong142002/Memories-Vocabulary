fbd956c80379bbe9fcd068546cd4608de772ecb4
Huong Le <huong.le@asnet.com.vn>
2023-05-17 09:21:59 +0700

    #23 Refactor: define error boundary component and wrap home page to catch error

--- react/practice-three/src/helpers/index.ts
+++ react/practice-three/src/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './handleErrors';
 export * from './convert';
+export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as validation } from './validators';
--- react/practice-three/src/pages/HomePage/index.tsx
+++ react/practice-three/src/pages/HomePage/index.tsx
@@ -3,13 +3,32 @@ import { ModalProvider } from '@contexts';
 
 // Layouts
 import { Header, HomeLayout } from '@layouts';
+import { ErrorBoundary } from '@helpers';
+import { Button, ModalNotification } from '@components';
 
 const HomePage = () => {
+  const handleCancel = () => {
+    console.log('cancel');
+  };
+
   return (
     <>
       <Header />
       <ModalProvider>
-        <HomeLayout />
+        <ErrorBoundary
+          fallback={
+            <ModalNotification
+              url='/icons/trash-icon.svg'
+              title='Ooops!'
+              description={'Something went wrong.'}
+              onCancel={handleCancel}
+            >
+              <Button label='Close' variant='tertiary' color='warning' size='lg' />
+            </ModalNotification>
+          }
+        >
+          <HomeLayout />
+        </ErrorBoundary>
       </ModalProvider>
     </>
   );
