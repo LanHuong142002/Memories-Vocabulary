6a7babe2c8c8f3a17504532e4900d05e44948a19
Huong Le <huong.le@asnet.com.vn>
2023-05-18 17:21:12 +0700

    #25 Refactor: remove redundant code, add type for status and type

--- react/practice-three/src/services/status.ts
+++ react/practice-three/src/services/status.ts
@@ -4,21 +4,24 @@ import { URL_API } from '@constants';
 // Helpers
 import { ResponseError, customMessageErrors } from '@helpers';
 
+// Interfaces
+import { ProductStatus } from '@interfaces';
+
 /**
  * @description function get all statuses
  *
  * @returns {Array} list statuses
  */
-const getStatuses = async <T>(): Promise<T[] | string> => {
+const getStatuses = async (): Promise<ProductStatus[] | string> => {
   try {
     const response = await fetch(`${URL_API.BASE_URL}${URL_API.STATUSES}`);
-    const status: T[] = await response.json();
+    const statuses: ProductStatus[] = await response.json();
 
     if (!response.ok) {
       const message = customMessageErrors(response);
       throw new ResponseError(message);
     }
-    return status;
+    return statuses;
   } catch (error) {
     return (error as ResponseError).message;
   }
