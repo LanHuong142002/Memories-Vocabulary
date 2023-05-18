711b8a5f9f60cdf2e88ad17e7b9c76f99741e0b5
Huong Le <huong.le@asnet.com.vn>
2023-05-18 17:24:57 +0700

    #13 Refactor: rename productsTable to productTable

--- react/practice-three/src/pages/HomePage/components/ProductTable/ProductRow.tsx
+++ react/practice-three/src/pages/HomePage/components/ProductTable/ProductRow.tsx
@@ -127,7 +127,7 @@ const ProductRow = ({
       <TableCell tagName='td'>
         <Image
           ref={iconImage}
-          url={loadImage('/icons/more.svg')}
+          url={loadImage('/icons/more-icon.svg')}
           size='xs'
           alt='icon more'
           isClickable
