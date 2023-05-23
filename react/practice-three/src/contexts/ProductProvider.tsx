5ce43002ff1c4c41f96ce5061f0423523f13cf83
Huong Le <huong.le@asnet.com.vn>
2023-05-23 15:48:07 +0700

    #27 Refactor: rename function in context

--- react/practice-three/src/contexts/ProductProvider.tsx
+++ react/practice-three/src/contexts/ProductProvider.tsx
@@ -1,133 +1,93 @@
-import {
-  ReactNode,
-  createContext,
-  useCallback,
-  useEffect,
-  useMemo,
-  useReducer,
-  useState,
-} from 'react';
+import { ReactNode, createContext, useCallback, useMemo, useState } from 'react';
 
 // Interfaces
 import { Product } from '@interfaces';
 
+// Constants
+import { URL_API } from '@constants';
+
 // Hooks
 import { useProduct } from '@hooks';
-
-// Stores
-import { State, initialState, reducer } from '@stores';
-
-// Constants
-import { ACTIONS } from '@constants';
 
 // Services
 import { deleteProduct, getProductsByParam, postProduct, updateProduct } from '@services';
 
-export interface Context extends State {
+import { mutate } from 'swr';
+
+export interface Context {
   products: Product[];
   messageError: string;
-  handleAddProduct: (product: Product) => void;
-  handleGetProducts: (products: Product[]) => void;
-  handleDeleteProduct: (id: string) => void;
-  handleUpdateProduct: (product: Product) => void;
-  handleSearchProducts: (param: string) => void;
-  handleSetMessageError: (message: string) => void;
+  onAddProduct: (product: Product) => void;
+  onDeleteProduct: (id: string) => void;
+  onUpdateProduct: (product: Product) => void;
+  onSearchProducts: (param: string) => void;
+  onSetMessageError: (message: string) => void;
 }
 
 export const ProductContext = createContext<Context>({} as Context);
 
 export const ProductProvider = ({ children }: { children: ReactNode }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { products } = state;
   const [messageError, setMessageError] = useState('');
-  const { data: items, error, isLoading } = useProduct();
+  const { data: products, error, isLoading } = useProduct();
 
   /**
    * @description function set message error
    */
-  const handleSetMessageError = useCallback((message: string) => {
+  const onSetMessageError = useCallback((message: string) => {
     setMessageError(message);
   }, []);
 
   /**
-   * @description function get products
-   */
-  const handleGetProducts = useCallback(() => {
-    dispatch({
-      type: ACTIONS.GET_PRODUCTS,
-      payload: items as Product[],
-    });
-  }, [items]);
-
-  /**
    * @description get products after search
    */
-  const handleSearchProducts = useCallback(async (param: string) => {
+  const onSearchProducts = useCallback(async (param: string) => {
     const response = await getProductsByParam(param);
 
     if (typeof response === 'string') {
       setMessageError(response);
     } else {
-      dispatch({
-        type: ACTIONS.GET_PRODUCTS,
-        payload: response,
-      });
+      mutate(`${URL_API.BASE_URL}${URL_API.PRODUCTS}?_expand=statuses&_expand=types`);
     }
   }, []);
 
   /**
    * @description function add new product
    */
-  const handleAddProduct = useCallback(async (product: Product) => {
+  const onAddProduct = useCallback(async (product: Product) => {
     const response = await postProduct(product);
 
     if (typeof response === 'string') {
       setMessageError(response);
     } else {
-      dispatch({
-        type: ACTIONS.ADD_PRODUCT,
-        payload: response,
-      });
+      mutate(`${URL_API.BASE_URL}${URL_API.PRODUCTS}`);
     }
   }, []);
 
   /**
    * @description function delete product
    */
-  const handleDeleteProduct = useCallback(async (id: string) => {
+  const onDeleteProduct = useCallback(async (id: string) => {
     const response = await deleteProduct(id);
 
     if (typeof response === 'string') {
       setMessageError(response);
     } else {
-      dispatch({
-        type: ACTIONS.DELETE_PRODUCT,
-        payload: id,
-      });
+      mutate(`${URL_API.BASE_URL}${URL_API.PRODUCTS}`);
     }
   }, []);
 
   /**
    * @description function update product
    */
-  const handleUpdateProduct = useCallback(async (product: Product) => {
+  const onUpdateProduct = useCallback(async (product: Product) => {
     const response = await updateProduct(product);
 
     if (typeof response === 'string') {
       setMessageError(response);
     } else {
-      dispatch({
-        type: ACTIONS.UPDATE_PRODUCT,
-        payload: response,
-      });
+      mutate(`${URL_API.BASE_URL}${URL_API.PRODUCTS}`);
     }
   }, []);
-
-  useEffect(() => {
-    if (items) {
-      handleGetProducts();
-    }
-  }, [handleGetProducts, items]);
 
   const value = useMemo(
     () => ({
@@ -135,24 +95,22 @@ export const ProductProvider = ({ children }: { children: ReactNode }) => {
       error,
       isLoading,
       messageError,
-      handleAddProduct,
-      handleGetProducts,
-      handleDeleteProduct,
-      handleUpdateProduct,
-      handleSearchProducts,
-      handleSetMessageError,
+      onAddProduct,
+      onDeleteProduct,
+      onUpdateProduct,
+      onSearchProducts,
+      onSetMessageError,
     }),
     [
       products,
       error,
       isLoading,
       messageError,
-      handleGetProducts,
-      handleDeleteProduct,
-      handleUpdateProduct,
-      handleAddProduct,
-      handleSearchProducts,
-      handleSetMessageError,
+      onDeleteProduct,
+      onUpdateProduct,
+      onAddProduct,
+      onSearchProducts,
+      onSetMessageError,
     ],
   );
 
