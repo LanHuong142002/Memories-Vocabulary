1f36f270ed019f453fe7489cf6b963004200c87c
Huong Le <huong.le@asnet.com.vn>
2023-05-23 12:09:33 +0700

    #27 Refactor: update product context

--- react/practice-three/src/contexts/ProductProvider.tsx
+++ react/practice-three/src/contexts/ProductProvider.tsx
@@ -1,4 +1,12 @@
-import { ReactNode, createContext, useEffect, useMemo, useReducer } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useState,
+} from 'react';
 
 // Interfaces
 import { Product } from '@interfaces';
@@ -7,65 +15,135 @@ import { Product } from '@interfaces';
 import { useProduct } from '@hooks';
 
 // Stores
-import { State, reducer } from '@stores';
+import { State, initialState, reducer } from '@stores';
 
 // Constants
 import { ACTIONS } from '@constants';
 
-export interface Context extends State {
-  addProduct: (product: Product) => Promise<void>;
-  deleteProduct: (id: string) => Promise<void>;
-  updateProduct: (product: Product[]) => Promise<void>;
-}
+// Services
+import { deleteProduct, getProductsByParam, postProduct, updateProduct } from '@services';
 
-const initialState: State = {
-  products: [],
-};
+export interface Context extends State {
+  products: Product[];
+  message: string;
+  handleAddProduct: (product: Product) => void;
+  handleGetProducts: (products: Product[]) => void;
+  handleDeleteProduct: (id: string) => void;
+  handleUpdateProduct: (product: Product) => void;
+  handleSearchProducts: (param: string) => void;
+}
 
 export const ProductContext = createContext<Context>({} as Context);
 
 export const ProductProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { products } = state;
+  const [message, setMessage] = useState('');
   const { data: items, error, isLoading } = useProduct();
 
-  useEffect(() => {
-    getProducts(items as Product[]);
-  }, [items]);
-
-  const getProducts = (products: Product[]) => {
+  /**
+   * @description function get products
+   */
+  const handleGetProducts = useCallback(() => {
     dispatch({
       type: ACTIONS.GET_PRODUCTS,
-      payload: products,
+      payload: items as Product[],
     });
-  };
+  }, [items]);
 
-  const addProduct = (product: Product) => {
-    dispatch({
-      type: ACTIONS.ADD_PRODUCT,
-      payload: product,
-    });
-  };
+  /**
+   * @description get products after search
+   */
+  const handleSearchProducts = useCallback(async (param: string) => {
+    const productItem = await getProductsByParam(param);
 
-  const deleteProduct = (id: string) => {
-    console.log('a', id);
-  };
+    if (typeof productItem === 'string') {
+      setMessage(productItem);
+    } else {
+      dispatch({
+        type: ACTIONS.GET_PRODUCTS,
+        payload: productItem,
+      });
+    }
+  }, []);
 
-  const updateProduct = (product: Product[]) => {
-    console.log('a', product);
-  };
+  /**
+   * @description function add new product
+   */
+  const handleAddProduct = useCallback(async (product: Product) => {
+    const productItem = await postProduct(product);
+
+    if (typeof productItem === 'string') {
+      setMessage(productItem);
+    } else {
+      dispatch({
+        type: ACTIONS.ADD_PRODUCT,
+        payload: productItem,
+      });
+    }
+  }, []);
+
+  /**
+   * @description function delete product
+   */
+  const handleDeleteProduct = useCallback(async (id: string) => {
+    const productItem = await deleteProduct(id);
+
+    if (typeof productItem === 'string') {
+      setMessage(productItem);
+    } else {
+      dispatch({
+        type: ACTIONS.DELETE_PRODUCT,
+        payload: id,
+      });
+    }
+  }, []);
+
+  /**
+   * @description function update product
+   */
+  const handleUpdateProduct = useCallback(async (product: Product) => {
+    const productItem = await updateProduct(product);
+
+    if (typeof productItem === 'string') {
+      setMessage(productItem);
+    } else {
+      dispatch({
+        type: ACTIONS.UPDATE_PRODUCT,
+        payload: productItem,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (items) {
+      handleGetProducts();
+    }
+  }, [handleGetProducts, items]);
 
   const value = useMemo(
     () => ({
       products,
       error,
       isLoading,
-      addProduct,
-      getProducts,
-      deleteProduct,
-      updateProduct,
+      message,
+      handleAddProduct,
+      handleGetProducts,
+      handleDeleteProduct,
+      handleUpdateProduct,
+      handleSearchProducts,
     }),
-    [products, error, isLoading, getProducts, deleteProduct, updateProduct, addProduct],
+    [
+      products,
+      error,
+      isLoading,
+      message,
+      handleGetProducts,
+      handleDeleteProduct,
+      handleUpdateProduct,
+      handleAddProduct,
+      handleSearchProducts,
+    ],
   );
 
   return <ProductContext.Provider value={value}>{children}</ProductContext.Provider>;
