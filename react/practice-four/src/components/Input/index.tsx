c596aec3d1c26c3e736ef3696aef141b8abb1a17
HuongLe <huong.le@asnet.com.vn>
2023-08-03 16:18:01 +0700

    #41 feat: add memo for input component

--- react/practice-four/src/components/Input/index.tsx
+++ react/practice-four/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, memo, useState } from 'react';
 
 // Styles
 import './index.css';
@@ -12,44 +12,39 @@ export interface InputProps {
   onChange: (event: ChangeEvent) => void;
 }
 
-export const Input = ({
-  placeholder,
-  value,
-  name,
-  error,
-  onChange,
-  variant = 'primary',
-}: InputProps) => {
-  const [isDefault, setIsDefault] = useState<boolean>(false);
+export const Input = memo(
+  ({ placeholder, value, name, error, onChange, variant = 'primary' }: InputProps) => {
+    const [isDefault, setIsDefault] = useState<boolean>(false);
 
-  const handleInputChange = (event: ChangeEvent) => {
-    // Because the input tertiary have 3 status: default, success, and failed
-    // So this state is check that if the input changed, the status just have two status left
-    setIsDefault(true);
-    if (onChange) {
-      onChange(event);
-    }
-  };
+    const handleInputChange = (event: ChangeEvent) => {
+      // Because the input tertiary have 3 status: default, success, and failed
+      // So this state is check that if the input changed, the status just have two status left
+      setIsDefault(true);
+      if (onChange) {
+        onChange(event);
+      }
+    };
 
-  return (
-    <div
-      className={`input-wrapper input-${variant} ${
-        isDefault && (error ? 'input-error' : 'input-success')
-      }`}
-    >
-      {variant !== 'primary' && <span className={error ? 'title-error' : ''}>Title</span>}
-      <input
-        className={`input input-${variant}`}
-        placeholder={placeholder}
-        value={value}
-        name={name}
-        onChange={handleInputChange}
-      />
-      {error && (
-        <div className='error-wrapper'>
-          <span className='error'>{error}</span>
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div
+        className={`input-wrapper input-${variant} ${
+          isDefault && (error ? 'input-error' : 'input-success')
+        }`}
+      >
+        {variant !== 'primary' && <span className={error ? 'title-error' : ''}>Title</span>}
+        <input
+          className={`input input-${variant}`}
+          placeholder={placeholder}
+          value={value}
+          name={name}
+          onChange={handleInputChange}
+        />
+        {error && (
+          <div className='error-wrapper'>
+            <span className='error'>{error}</span>
+          </div>
+        )}
+      </div>
+    );
+  },
+);
