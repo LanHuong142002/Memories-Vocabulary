70f8488e13bab17e05a9a06d569bf06afaa2d7ad
HuongLe <huong.le@asnet.com.vn>
2023-08-04 09:14:37 +0700

    #41 feat: update CSS dark theme for table components

--- react/practice-four/src/components/Table/TableCell/index.tsx
+++ react/practice-four/src/components/Table/TableCell/index.tsx
@@ -6,14 +6,17 @@ import './index.css';
 interface TableCellProps {
   tagName?: 'th' | 'td';
   children?: ReactNode;
+  color?: 'success' | 'failed';
 }
 
-export const TableCell = memo(({ children, tagName = 'td' }: TableCellProps): ReactElement => {
-  const TagName = tagName;
+export const TableCell = memo(
+  ({ children, tagName = 'td', color }: TableCellProps): ReactElement => {
+    const TagName = tagName;
 
-  return (
-    <TagName className='table-cell' data-testid='table-cell'>
-      {children}
-    </TagName>
-  );
-});
+    return (
+      <TagName className={`table-cell table-cell-${color ? color : ''}`} data-testid='table-cell'>
+        {children}
+      </TagName>
+    );
+  },
+);
--- react/practice-four/src/components/Table/index.tsx
+++ react/practice-four/src/components/Table/index.tsx
@@ -4,9 +4,12 @@ import { ReactElement, ReactNode, memo } from 'react';
 import './index.css';
 
 interface TableProps {
+  theme?: 'light' | 'dark';
   children: ReactNode;
 }
 
 export const Table = memo(
-  ({ children }: TableProps): ReactElement => <table className='table-wrapper'>{children}</table>,
+  ({ children, theme = 'light' }: TableProps): ReactElement => (
+    <table className={`table-wrapper table-wrapper-${theme}`}>{children}</table>
+  ),
 );
