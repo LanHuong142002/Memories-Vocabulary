f8cc98bc03581f9fa23871a57b49bee7c66af527
HuongLe <huong.le@asnet.com.vn>
2023-08-07 15:26:52 +0700

    #41 feat: define mocks data for re-using

--- react/practice-four/src/components/TableResult/TableResult.stories.tsx
+++ react/practice-four/src/components/TableResult/TableResult.stories.tsx
@@ -3,6 +3,9 @@ import type { Meta, StoryObj } from '@storybook/react';
 // Components
 import { TableResult } from '@components';
 
+// Mocks
+import { mockTableResult } from '@mocks';
+
 const meta: Meta<typeof TableResult> = {
   title: 'PracticeFour/TableResult',
   component: TableResult,
@@ -21,36 +24,12 @@ const meta: Meta<typeof TableResult> = {
   ],
 };
 
-const mockData = [
-  {
-    isSuccess: true,
-    no: 1,
-    native: 'pen',
-    translation: 'cay but',
-    answer: 'cay but',
-  },
-  {
-    isSuccess: false,
-    no: 2,
-    native: 'book',
-    translation: 'quyen sach',
-    answer: 'cuon sach',
-  },
-  {
-    isSuccess: true,
-    no: 3,
-    native: 'notebook',
-    translation: 'quyen vo',
-    answer: 'quyen vo',
-  },
-];
-
 type Story = StoryObj<typeof TableResult>;
 
 export const Default: Story = {
   args: {
     theme: 'light',
-    result: mockData,
+    result: mockTableResult,
   },
 };
 
--- react/practice-four/src/components/TableResult/__tests__/TableResult.test.tsx
+++ react/practice-four/src/components/TableResult/__tests__/TableResult.test.tsx
@@ -1,40 +1,23 @@
 import { render } from '@testing-library/react';
+
+// Mocks
+import { mockTableResult } from '@mocks';
+
+// Components
 import { TableResult } from '..';
 
 describe('Should test table result component', () => {
-  const mockData = [
-    {
-      isSuccess: true,
-      no: 1,
-      native: 'pen',
-      translation: 'cay but',
-      answer: 'cay but',
-    },
-    {
-      isSuccess: false,
-      no: 2,
-      native: 'book',
-      translation: 'quyen sach',
-      answer: 'cuon sach',
-    },
-    {
-      isSuccess: true,
-      no: 3,
-      native: 'notebook',
-      translation: 'quyen vo',
-      answer: 'quyen vo',
-    },
-  ];
-
   it('Should render table result component', () => {
-    const { container, getAllByRole } = render(<TableResult result={mockData} />);
+    const { container, getAllByRole } = render(<TableResult result={mockTableResult} />);
 
     expect(container).toBeInTheDocument();
     expect(getAllByRole('row').length).toBe(8);
   });
 
   it('Should render table result dark component', () => {
-    const { container, getAllByRole } = render(<TableResult result={mockData} theme='dark' />);
+    const { container, getAllByRole } = render(
+      <TableResult result={mockTableResult} theme='dark' />,
+    );
 
     expect(container).toBeInTheDocument();
     expect(getAllByRole('row').length).toBe(8);
