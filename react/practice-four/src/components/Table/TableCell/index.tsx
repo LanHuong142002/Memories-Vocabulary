bdc8f276de7ff59da6ba05f3dffeb137ea0450fe
Huong Le <huong.le@asnet.com.vn>
2023-08-07 11:46:16 +0700

    #41 feat: remove redundant prop of table cell

--- react/practice-four/src/components/Table/TableCell/index.tsx
+++ react/practice-four/src/components/Table/TableCell/index.tsx
@@ -2,7 +2,6 @@ import { ReactElement, ReactNode, memo } from 'react';
 // Styles
 import './index.css';
 interface TableCellProps {
-  hasBorder?: boolean;
   rowspan?: number;
   colspan?: number;
   tagName?: 'th' | 'td';
@@ -10,20 +9,11 @@ interface TableCellProps {
   children?: ReactNode;
 }
 export const TableCell = memo(
-  ({
-    hasBorder = false,
-    rowspan,
-    colspan,
-    children,
-    tagName = 'td',
-    color,
-  }: TableCellProps): ReactElement => {
+  ({ rowspan, colspan, children, tagName = 'td', color }: TableCellProps): ReactElement => {
     const TagName = tagName;
     return (
       <TagName
-        className={`table-cell table-cell-${color ? color : ''} ${
-          hasBorder ? 'table-cell-border' : ''
-        }`}
+        className={`table-cell table-cell-${color ? color : ''}`}
         data-testid='table-cell'
         rowSpan={rowspan}
         colSpan={colspan}
