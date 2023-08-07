76f2755203fda895e1046484ef57140561fb1a46
Huong Le <huong.le@asnet.com.vn>
2023-08-07 11:45:17 +0700

    #41 feat: update test of input, table and topic component

--- react/practice-four/src/components/Table/__tests__/Table.test.tsx
+++ react/practice-four/src/components/Table/__tests__/Table.test.tsx
@@ -3,9 +3,9 @@ import { render } from '@testing-library/react';
 import { Table } from '..';
 
 describe('Test table component', () => {
-  it('Should render table component', () => {
+  it('Should render table component with border', () => {
     const { getByTestId } = render(
-      <Table>
+      <Table hasBorderCell={true}>
         <thead data-testid='table-header'>
           <tr>
             <th>table header</th>
@@ -26,5 +26,26 @@ describe('Test table component', () => {
     expect(tableHeader.children.length).toBe(1);
     expect(tableBody).toBeInTheDocument();
     expect(tableBody.children.length).toBe(1);
+  });
+
+  it('Should render table component without border', () => {
+    const { getByTestId } = render(
+      <Table theme='dark'>
+        <thead data-testid='table-header'>
+          <tr>
+            <th>table header</th>
+          </tr>
+        </thead>
+        <tbody data-testid='table-body'>
+          <tr>
+            <td>cell 1</td>
+            <td>cell 2</td>
+          </tr>
+        </tbody>
+      </Table>,
+    );
+    const tableHeader = getByTestId('table-header');
+
+    expect(tableHeader).toBeInTheDocument();
   });
 });
