69a7b98e711b71cc32c754f753d729b85e715cf6
HuongLe <huong.le@asnet.com.vn>
2023-08-08 08:57:03 +0700

    #45 feat: define error page to show when error boundary catched error

--- react/practice-four/src/main.tsx
+++ react/practice-four/src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 // Components
 import { App } from '@App';
+import { ErrorBoundary } from '@components';
+import { ErrorPage } from '@pages';
 
 // Styles
 import './styles/main.css';
@@ -11,7 +13,9 @@ import './styles/main.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <App />
+      <ErrorBoundary fallback={<ErrorPage />}>
+        <App />
+      </ErrorBoundary>
     </Router>
   </StrictMode>,
 );
