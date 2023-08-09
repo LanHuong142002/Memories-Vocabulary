7bd7186311b59f2d783038a99cc58508d70f692d
HuongLe <huong.le@asnet.com.vn>
2023-08-09 15:46:21 +0700

    #46 feat: remove prop theme and update CSS with common variables of each theme

--- react/practice-four/src/components/Table/index.tsx
+++ react/practice-four/src/components/Table/index.tsx
@@ -5,15 +5,12 @@ import './index.css';
 
 interface TableProps {
   hasBorderCell?: boolean;
-  theme?: 'light' | 'dark';
   children: ReactNode;
 }
 
 const Table = memo(
-  ({ hasBorderCell = false, children, theme = 'light' }: TableProps): ReactElement => (
-    <table
-      className={`table-wrapper table-wrapper-${theme} ${hasBorderCell ? 'table-cell-border' : ''}`}
-    >
+  ({ hasBorderCell = false, children }: TableProps): ReactElement => (
+    <table className={`table-wrapper ${hasBorderCell ? 'table-cell-border' : ''}`}>
       {children}
     </table>
   ),
--- react/practice-four/src/components/Typography/index.tsx
+++ react/practice-four/src/components/Typography/index.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from 'react';
 import './index.css';
 
 interface TypographyProps {
-  theme?: 'light' | 'dark';
   size?: 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
   color?: 'primary' | 'secondary' | 'tertiary';
   tagName?: 'p' | 'span';
@@ -12,15 +11,12 @@ interface TypographyProps {
 }
 
 const Typography = ({
-  theme = 'light',
   size = 'xs',
   color = 'primary',
   tagName: TagName = 'p',
   children,
 }: TypographyProps) => (
-  <TagName
-    className={`typography typography-${size} typography-color-${color} typography-${theme}`}
-  >
+  <TagName className={`typography typography-${size} typography-color-${color}`}>
     {children}
   </TagName>
 );
