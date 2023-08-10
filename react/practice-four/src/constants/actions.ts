8247b13f52b1bf98c518e8c129842a785989aecb
HuongLe <huong.le@asnet.com.vn>
2023-08-10 12:05:51 +0700

    #47 feat: rename constants

--- react/practice-four/src/constants/actions.ts
+++ react/practice-four/src/constants/actions.ts
@@ -1,9 +1,9 @@
-export const ACTIONS_TOPIC = {
+export const TOPIC_ACTIONS = {
   GET: 'get_topics',
   POST: 'post_topic',
 };
 
-export const ACTIONS_VOCABULARY = {
+export const VOCABULARY_ACTIONS = {
   GET: 'get_vocabularies',
   POST: 'post_vocabulary',
   PUT: 'put_vocabulary',
