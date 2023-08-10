de4f712195625ec7342ae5f5db3edacad6dbf9da
HuongLe <huong.le@asnet.com.vn>
2023-08-10 16:22:08 +0700

    #47 feat: create common actions for vocabulary and topic actions

--- react/practice-four/src/constants/actions.ts
+++ react/practice-four/src/constants/actions.ts
@@ -1,8 +1,12 @@
+const COMMON_ACTIONS = {
+  PENDING: 'request_pending',
+  FAILED: 'request_failed',
+};
+
 export const TOPIC_ACTIONS = {
-  PENDING: 'request_topics_pending',
-  FAILED: 'request_topics_failed',
   GET: 'get_topics',
   POST: 'post_topic',
+  ...COMMON_ACTIONS,
 };
 
 export const VOCABULARY_ACTIONS = {
@@ -10,4 +14,5 @@ export const VOCABULARY_ACTIONS = {
   POST: 'post_vocabulary',
   PUT: 'put_vocabulary',
   DELETE: 'delete_vocabulary',
+  ...COMMON_ACTIONS,
 };
