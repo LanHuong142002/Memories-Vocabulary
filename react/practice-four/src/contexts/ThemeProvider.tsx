76138e809e18b14daa59612464ca56a5879ed733
HuongLe <huong.le@asnet.com.vn>
2023-08-22 11:08:10 +0700

    feat: add type for getItems storage

--- react/practice-four/src/contexts/ThemeProvider.tsx
+++ react/practice-four/src/contexts/ThemeProvider.tsx
@@ -25,7 +25,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   }, []);
 
   const initialize = useCallback(() => {
-    const themeColor = getItems<string>(STORAGE_KEYS.THEME) as 'light' | 'dark';
+    const themeColor = getItems<'light' | 'dark'>(STORAGE_KEYS.THEME);
     setIsInitialized(true);
 
     if (themeColor) {
