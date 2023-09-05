e8a699f19c1c6d999fea2409458b76ca74a29004
HuongLe <huong.le@asnet.com.vn>
2023-09-05 16:40:08 +0700

    #58 feat: update typo for message errors

--- react/practice-four/src/constants/messages.ts
+++ react/practice-four/src/constants/messages.ts
@@ -1,6 +1,6 @@
 export const MESSAGE_ERRORS = {
   REQUIRED: 'This field is required',
-  MIN_LENGTH: 'Minimum length is should 3',
-  ALPHABETS: 'Only alphabets are allow',
+  MIN_LENGTH: 'Minimum length should be 3',
+  ALPHABETS: 'Only alphabets are allowed',
   EXISTED: 'This vocabulary already exists!',
 };
