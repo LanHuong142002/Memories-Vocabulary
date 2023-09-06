82c5d98250a13e86b5ceabc48e404071296700c6
HuongLe <huong.le@asnet.com.vn>
2023-09-06 09:48:03 +0700

    #58 feat: add suspense for pages

--- react/practice-four/src/main.tsx
+++ react/practice-four/src/main.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // Contexts
 import { ThemeProvider } from '@contexts';
 
+// Constants
+import { SPINNER_VARIANT } from '@constants';
+
 // Components
 import { App } from '@App';
-import { ErrorBoundary } from '@components';
+import { ErrorBoundary, Spinner } from '@components';
 import { Error } from '@pages';
 
 // Styles
@@ -17,9 +20,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <ErrorBoundary fallback={<Error />}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <Suspense fallback={<Spinner variant={SPINNER_VARIANT.PRIMARY} />}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </Suspense>
       </ErrorBoundary>
     </Router>
   </StrictMode>,
