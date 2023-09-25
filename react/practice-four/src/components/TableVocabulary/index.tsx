484001ab94f42dc42fa01ceff097bbb9689133e8
HuongLe <huong.le@asnet.com.vn>
2023-09-25 15:10:01 +0700

    #65 feat: update CSS selector for table result and table vocabulary

--- react/practice-four/src/components/TableVocabulary/index.tsx
+++ react/practice-four/src/components/TableVocabulary/index.tsx
@@ -75,7 +75,7 @@ const TableVocabulary = memo(
       <Box
         className='table-body'
         sx={(theme: MantineTheme) => ({
-          '.row:nth-child(even):hover, .row:nth-child(odd)': {
+          '.row:nth-of-type:first-of-type(even):hover, .row:nth-of-type:first-of-type(odd)': {
             backgroundColor: getColorScheme(
               theme.colorScheme,
               theme.colors.dark[2],
